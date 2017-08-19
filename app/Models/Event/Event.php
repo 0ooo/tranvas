2d9cf4c1d839cd6ca81f9a30d195ec4d29d245a8
Pavel Gontarenko <pavgont@gmail.com>
2017-08-19 22:46:06 +0300

    Добавил модель и контроллер Event

---
+++ app/Models/Event/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Event;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /**
+     * Поля, защищенные на запись.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
