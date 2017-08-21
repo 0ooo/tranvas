c6241478426a5376e0c38a353b3433b6580bc9bc
Pavel Gontarenko <pavgont@gmail.com>
2017-08-21 09:32:39 +0300

    - Добавлена фабрика для Event

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,10 @@
 |
 */
 
+use App\Models\Event\Event;
+use App\User;
+use Carbon\Carbon;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
@@ -20,5 +24,23 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Event::class, function (Faker\Generator $faker) {
+
+    $start_date = Carbon::now()->addDays($faker->randomElement([1,2,3,4,5,6,7,8,9]));
+    $end_date = $start_date->copy()->addDays($faker->randomElement([1,2,3,4,5,6,7,8,9]));
+
+    return [
+        'title'       => $faker->sentence(5),
+        'description' => $faker->paragraph(5),
+        'address'     => $faker->address,
+        'lat'         => $faker->latitude,
+        'long'        => $faker->longitude,
+        'start_date'  => $start_date->format('Y-m-d'),
+        'end_date'    => $end_date->format('Y-m-d'),
+        'user_id'     => factory(User::class)->create()->id,
     ];
 });
