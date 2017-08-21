1f3b79fae5b0776f194b9c33b1c31ef9e3b60f0e
Pavel Gontarenko <pavgont@gmail.com>
2017-08-21 09:25:16 +0300

    - Добавлена связь события с пользователем

--- app/Models/Event/Event.php
+++ app/Models/Event/Event.php
@@ -2,8 +2,15 @@
 
 namespace App\Models\Event;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $creator
+ * @property \Carbon\Carbon $created_at
+ * @property int $id
+ * @property \Carbon\Carbon $updated_at
+ */
 class Event extends Model
 {
     /**
@@ -12,4 +19,14 @@ class Event extends Model
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * Связь с таблицей 'users'.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
