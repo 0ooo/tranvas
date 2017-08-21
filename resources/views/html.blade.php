0adf262e19a5cfe9690f7a0c07fa58e5b23ada7f
Pavel Gontarenko <pavgont@gmail.com>
2017-08-21 10:57:32 +0300

    - Вынес навигацию в отдельный файл. - Ссылка в навигации на events.

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -6,10 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    @yield('header-styles');
+    @yield('header-styles')
     <title>Tranvas</title>
 </head>
 <body>
+    @include('layouts.nav')
     <div class="container">
 
         <div class="content-container">
